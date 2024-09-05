21b31c817733455e9c1ddca9b11b929535a3bc78
ErmanZaimi <138713084+ErmanZaimi@users.noreply.github.com>
2024-09-05 20:38:42 +0000

    added nav and side bar

--- src/app/modules/contracts/pages/new-page/new-page.component.ts
+++ src/app/modules/contracts/pages/new-page/new-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../../layout/components/footer/footer.component';
+import { NavbarComponent } from 'src/app/modules/layout/components/navbar/navbar.component';
+import { SidebarComponent } from 'src/app/modules/layout/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styleUrls: ['./new-page.component.scss']
+  styleUrls: ['./new-page.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent, RouterOutlet, FooterComponent, ReactiveFormsModule],
 })
 export class NewPageComponent {
   form: FormGroup;
